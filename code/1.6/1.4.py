4db746f5d6c9d5d5ce0dd6c71789a2d974252464
yuxiang <1979202286@qq.com>
2025-05-15 22:58:13 +0800

    第一次提交

---
+++ code/1.6/1.4.py
@@ -0,0 +1,13 @@
+# CalRunTime.py
+
+import time
+limit = 10 * 1000 * 1000
+
+start = time.perf_counter()
+while True:
+    limit = limit - 1
+    if limit <= 0:
+        break
+
+delta = time.perf_counter() - start
+print("程序运行时间是：{}秒".format(delta))
